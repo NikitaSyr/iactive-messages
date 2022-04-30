fe764b30eb013c93af2975999fc6cdec2343b63c
NikitaSyr <galaxynik7@gmail.com>
2022-05-01 03:04:03 +0800

    GitHub pages staticData added

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,6 @@
 import axios, {AxiosInstance, AxiosRequestConfig} from "axios";
 import {IMessage} from "../types/types";
+import {staticData} from "./staticData";
 
 const config: AxiosRequestConfig = {
     baseURL: `http://f0665380.xsph.ru/`,
@@ -11,13 +12,20 @@ const config: AxiosRequestConfig = {
 const instance: AxiosInstance = axios.create(config);
 
 export const messagesAPI = {
-    async postFirstMessages(): Promise<Array<IMessage>> {
-        const response = await instance.post("", {
-            messageId: 0,
-            actionName: "MessagesLoad"
-            }
-        );
-        return response.data.Messages;
+    async postFirstMessages() {
+        try {
+            const response = await instance.post("", {
+                    messageId: 0,
+                    actionName: "MessagesLoad"
+                }
+            );
+            return response.data.Message;
+        }
+        catch (e)  {
+            console.log("Вы видите статичную версию приложения. Для просмотра динамической работы с API клонируйте репозиторий");
+            return staticData as Array<IMessage>;
+        }
+
     },
     async postNewMessage(id: string): Promise<Array<IMessage> | undefined> {
         const response = await instance.post("", {
